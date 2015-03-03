105f2a2fac23b3f62155c8f0ffaa0486df8a80eb
Kevin Ndung'u <kevgathuku@gmail.com>
2015-03-03 17:15:17 +0300

    Add a basic test for the project structure
    This very basically compares the sample app with the generated app
    and ensures the tree structure is the same

---
+++ tests/sample_app/my-awesome-project/my-awesome-project/settings/local.py
@@ -0,0 +1,9 @@
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'CHANGE THIS!!!'
