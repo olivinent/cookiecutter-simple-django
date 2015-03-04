8c68f09c571326356fe91987b28771e44c540486
Kevin Ndung'u <kevgathuku@gmail.com>
2015-03-04 07:23:11 +0300

    Fix PEP8 warnings
    Refactor to comply with PEP8 requirements

--- tests/test_file_creation.py
+++ tests/test_file_creation.py
@@ -5,10 +5,14 @@ import subprocess
 import tempfile
 import unittest
 
+
 class TestTreeCreation(unittest.TestCase):
 
     destpath = os.path.join(tempfile.gettempdir(), 'my-awesome-project')
-    comparepath = os.path.join(os.path.dirname(__file__), 'sample_app', 'my-awesome-project')
+    comparepath = os.path.join(
+        os.path.dirname(__file__),
+        'sample_app',
+        'my-awesome-project')
 
     def setUp(self):
         if os.path.exists(self.destpath):
@@ -16,13 +20,16 @@ class TestTreeCreation(unittest.TestCase):
 
     def test_files_created(self):
         status = subprocess.call(
-            ['cookiecutter', '--no-input', os.path.dirname(os.path.dirname(__file__))])
+            ['cookiecutter', '--no-input', os.path.dirname(
+                os.path.dirname(__file__))])
 
         if status == 0:
             shutil.move(
-                os.path.join(os.path.dirname(os.path.dirname(__file__)),'my-awesome-project'),
-                self.destpath
-                )
+                os.path.join(
+                    os.path.dirname(os.path.dirname(__file__)),
+                    'my-awesome-project'),
+                self.destpath)
+
         self.assertEqual(
             filecmp.dircmp(
                 self.comparepath,
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
@@ -4,13 +4,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'tt.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-) 
+)
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
