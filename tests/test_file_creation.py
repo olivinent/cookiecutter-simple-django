204d147b1f5cc257b3020abda5877d750c2e5de7
Kevin Ndung'u <kevgathuku@gmail.com>
2015-03-04 07:59:14 +0300

    Refactor paths building in test script

--- tests/test_file_creation.py
+++ tests/test_file_creation.py
@@ -1,39 +1,34 @@
 import filecmp
-import os
 import shutil
 import subprocess
 import tempfile
 import unittest
+from os.path import abspath, dirname, exists, join
 
 
 class TestTreeCreation(unittest.TestCase):
 
-    destpath = os.path.join(tempfile.gettempdir(), 'my-awesome-project')
-    comparepath = os.path.join(
-        os.path.dirname(__file__),
+    destpath = join(tempfile.gettempdir(), 'my-awesome-project')
+    comparepath = join(
+        dirname(__file__),
         'sample_app',
         'my-awesome-project')
 
     def setUp(self):
-        if os.path.exists(self.destpath):
+        if exists(self.destpath):
             shutil.rmtree(self.destpath)
 
     def test_files_created(self):
         status = subprocess.call(
-            ['cookiecutter', '--no-input', os.path.dirname(
-                os.path.dirname(__file__))])
+            ['cookiecutter', '--no-input', dirname(dirname(__file__))])
 
         if status == 0:
             shutil.move(
-                os.path.join(
-                    os.path.dirname(os.path.dirname(__file__)),
-                    'my-awesome-project'),
+                join(dirname(dirname(__file__)), 'my-awesome-project'),
                 self.destpath)
 
         self.assertEqual(
-            filecmp.dircmp(
-                self.comparepath,
-                self.destpath).diff_files,
+            filecmp.dircmp(self.comparepath, self.destpath).diff_files,
             [])
 
 if __name__ == '__main__':
